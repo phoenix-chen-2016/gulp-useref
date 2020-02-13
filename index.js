2e39c2b4a6a1207a5e807c37b5c3199c55ca3de2
Phoenix <fphoenix923@hotmail.com>
2020-02-13 22:56:52 +0800

    fix multiple useref work fail.

--- index.js
+++ index.js
@@ -8,190 +8,190 @@ const addFilesFromExtStreams = require('./lib/addFilesFromExtStreams');
 const addHtmlToStream = require('./lib/addHtmlToStream');
 const unprocessedCounter = require('./lib/unprocessedCounter')();
 const end = require('./lib/end')();
-const additionalFiles = [];
-let transforms;
-let pluginOptions;
 
-function handleAdditionalStreams(additionalStreams) {
-    let _additionalStreams = additionalStreams;
+module.exports = function (options) {
+    const additionalFiles = [];
+    let transforms = Array.prototype.slice.call(arguments, 1);
+    let pluginOptions = options || {};
 
-    if (!Array.isArray(additionalStreams)) {
-        _additionalStreams = [ additionalStreams ];
-    }
+    function handleAdditionalStreams(additionalStreams) {
+        let _additionalStreams = additionalStreams;
 
-    // filters stream to select needed files
-    return _additionalStreams.map(stream => stream.pipe(es.through(file => {
-        additionalFiles.push(file);
-    })));
-}
-
-function addAssetsToStream(paths, files) {
-    const self = this;
-    const gulpif = require('gulp-if');
-    const concat = require('gulp-concat');
-    const vfs = require('vinyl-fs');
-    const extend = require('extend');
-    let src;
-    let globs;
-    const name = paths.name;
-    const basePath = paths.basePath;
-    const filepaths = files[name].assets;
-    const type = paths.type;
-    const options = extend({}, pluginOptions);
-    const gulpConcatOptions = {};
-
-    if (!filepaths.length) {
-        return;
-    }
-
-    unprocessedCounter.increment();
-
-    // Get relative file paths and join with search paths to send to vinyl-fs
-    globs = filepaths
-        .filter(url => !/^(?:\w+:)?\/\//.test(url)) // test if url is relative
-        .map(filepath => {
-            paths.filepath = filepath;
-
-            return getGlobs(paths, files);
-        });
-
-    src = vfs.src(globs, {
-        base: basePath,
-        nosort: true
-    })
-        .on('error', err => {
-            self.emit('error', new Error(err));
-        });
-
-    // add files from external streams
-    src = addFilesFromExtStreams.call(self, additionalFiles, globs, src);
-
-    // If any external transforms were included, pipe all files to them first
-    transforms.forEach(fn => {
-        src = src.pipe(fn(name));
-    });
-
-    // option for newLine in gulp-concat
-    if (Object.prototype.hasOwnProperty.call(options, 'newLine')) {
-        if (options.newLine === ';' && type === 'css') {
-            options.newLine = null;
+        if (!Array.isArray(additionalStreams)) {
+            _additionalStreams = [additionalStreams];
         }
-        gulpConcatOptions.newLine = options.newLine;
+
+        // filters stream to select needed files
+        return _additionalStreams.map(stream => stream.pipe(es.through(file => {
+            additionalFiles.push(file);
+        })));
     }
 
-    // Add assets to the stream
-    // If noconcat option is false, concat the files first.
-    src
-        .pipe(gulpif(!options.noconcat, concat(name, gulpConcatOptions)))
-        .pipe(through.obj((newFile, encoding, callback) => {
-            // specify an output path relative to the cwd
-            if (options.base) {
-                newFile.path = path.join(options.base, name);
-                newFile.base = options.base;
-            }
+    function addAssetsToStream(paths, files) {
+        const self = this;
+        const gulpif = require('gulp-if');
+        const concat = require('gulp-concat');
+        const vfs = require('vinyl-fs');
+        const extend = require('extend');
+        let src;
+        let globs;
+        const name = paths.name;
+        const basePath = paths.basePath;
+        const filepaths = files[name].assets;
+        const type = paths.type;
+        const options = extend({}, pluginOptions);
+        const gulpConcatOptions = {};
 
-            // add file to the asset stream
-            self.push(newFile);
-            callback();
-        }))
-        .on('finish', () => {
-            const unprocessed = unprocessedCounter.decrement();
-
-            if (unprocessed === 0 && end.get()) {
-                // end the asset stream
-                end.fn();
-            }
-        });
-}
-
-function processAssets({ cwd }, basePath, data) {
-    const self = this;
-    const types = pluginOptions.types || [ 'css', 'js' ];
-
-    types.forEach(type => {
-        const files = data[type];
-        let name;
-
-        if (!files) {
+        if (!filepaths.length) {
             return;
         }
 
-        for (name in files) {
-            addAssetsToStream.call(self, {
-                name,
-                basePath,
-                searchPath: pluginOptions.searchPath,
-                cwd,
-                transformPath: pluginOptions.transformPath,
-                type
-            }, files);
+        unprocessedCounter.increment();
+
+        // Get relative file paths and join with search paths to send to vinyl-fs
+        globs = filepaths
+            .filter(url => !/^(?:\w+:)?\/\//.test(url)) // test if url is relative
+            .map(filepath => {
+                paths.filepath = filepath;
+
+                return getGlobs(paths, files);
+            });
+
+        src = vfs.src(globs, {
+            base: basePath,
+            nosort: true
+        })
+            .on('error', err => {
+                self.emit('error', new Error(err));
+            });
+
+        // add files from external streams
+        src = addFilesFromExtStreams.call(self, additionalFiles, globs, src);
+
+        // If any external transforms were included, pipe all files to them first
+        transforms.forEach(fn => {
+            src = src.pipe(fn(name));
+        });
+
+        // option for newLine in gulp-concat
+        if (Object.prototype.hasOwnProperty.call(options, 'newLine')) {
+            if (options.newLine === ';' && type === 'css') {
+                options.newLine = null;
+            }
+            gulpConcatOptions.newLine = options.newLine;
         }
-    });
-}
 
-module.exports = function (options) {
-    const opts = options || {};
-    let waitForAssets;
-    let additionalStreams;
+        // Add assets to the stream
+        // If noconcat option is false, concat the files first.
+        src
+            .pipe(gulpif(!options.noconcat, concat(name, gulpConcatOptions)))
+            .pipe(through.obj((newFile, encoding, callback) => {
+                // specify an output path relative to the cwd
+                if (options.base) {
+                    newFile.path = path.join(options.base, name);
+                    newFile.base = options.base;
+                }
 
-    pluginOptions = opts;
-    transforms = Array.prototype.slice.call(arguments, 1);
+                // add file to the asset stream
+                self.push(newFile);
+                callback();
+            }))
+            .on('finish', () => {
+                const unprocessed = unprocessedCounter.decrement();
 
-    // If any external streams were included, add matched files to src
-    if (opts.additionalStreams) {
-        additionalStreams = handleAdditionalStreams(opts.additionalStreams);
-
-        // If we have additional streams, wait for them to run before continuing
-        waitForAssets = es.merge(additionalStreams).pipe(through.obj());
-    } else {
-        // Else, create a fake stream
-        waitForAssets = through.obj();
+                if (unprocessed === 0 && end.get()) {
+                    // end the asset stream
+                    end.fn();
+                }
+            });
     }
 
-    return through.obj(function (file, enc, cb) {
+    function processAssets({ cwd }, basePath, data) {
         const self = this;
+        const types = pluginOptions.types || ['css', 'js'];
 
-        waitForAssets.pipe(es.wait(() => {
-            let output;
+        types.forEach(type => {
+            const files = data[type];
+            let name;
 
-            // Cache the file base path relative to the cwd
-            // Use later when it could be dropped
-            const _basePath = path.dirname(file.path);
-
-            if (file.isNull()) {
-                return cb(null, file);
+            if (!files) {
+                return;
             }
 
-            if (file.isStream()) {
-                return cb(new PluginError('gulp-useref', 'Streaming not supported'));
+            for (name in files) {
+                addAssetsToStream.call(self, {
+                    name,
+                    basePath,
+                    searchPath: pluginOptions.searchPath,
+                    cwd,
+                    transformPath: pluginOptions.transformPath,
+                    type
+                }, files);
             }
+        });
+    }
 
-            output = useref(file.contents.toString(), opts);
+    return (function () {
+        const opts = pluginOptions;
+        let waitForAssets;
+        let additionalStreams;
 
-            addHtmlToStream.call(self, file, output[0]);
+        // If any external streams were included, add matched files to src
+        if (opts.additionalStreams) {
+            additionalStreams = handleAdditionalStreams(opts.additionalStreams);
 
-            if (!opts.noAssets) {
-                processAssets.call(self, file, _basePath, output[1]);
-            }
-
-            return cb();
-        }));
-
-        // If no external streams were included,
-        // emit 'end' on the empty stream
-        if (!additionalStreams) {
-            waitForAssets.emit('end');
-        }
-    }, cb => {
-        const unprocessed = unprocessedCounter.get();
-        let fn = () => {};
-
-        end.set(cb);
-
-        if (unprocessed === 0) {
-            fn = cb;
+            // If we have additional streams, wait for them to run before continuing
+            waitForAssets = es.merge(additionalStreams).pipe(through.obj());
+        } else {
+            // Else, create a fake stream
+            waitForAssets = through.obj();
         }
 
-        return fn();
-    });
+        return through.obj(function (file, enc, cb) {
+            const self = this;
+
+            waitForAssets.pipe(es.wait(() => {
+                let output;
+
+                // Cache the file base path relative to the cwd
+                // Use later when it could be dropped
+                const _basePath = path.dirname(file.path);
+
+                if (file.isNull()) {
+                    return cb(null, file);
+                }
+
+                if (file.isStream()) {
+                    return cb(new PluginError('gulp-useref', 'Streaming not supported'));
+                }
+
+                output = useref(file.contents.toString(), opts);
+
+                addHtmlToStream.call(self, file, output[0]);
+
+                if (!opts.noAssets) {
+                    processAssets.call(self, file, _basePath, output[1]);
+                }
+
+                return cb();
+            }));
+
+            // If no external streams were included,
+            // emit 'end' on the empty stream
+            if (!additionalStreams) {
+                waitForAssets.emit('end');
+            }
+        }, cb => {
+            const unprocessed = unprocessedCounter.get();
+            let fn = () => { };
+
+            end.set(cb);
+
+            if (unprocessed === 0) {
+                fn = cb;
+            }
+
+            return fn();
+        });
+    })();
 };
